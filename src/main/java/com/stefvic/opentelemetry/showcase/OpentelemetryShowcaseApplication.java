b11bbdf7f1de828e752696014b6511238f37cb1d
Victor Stefoglo <victor.stefoglo@gmail.com>
2021-04-21 12:40:14 -0400

    First commit

---
+++ src/main/java/com/stefvic/opentelemetry/showcase/OpentelemetryShowcaseApplication.java
@@ -0,0 +1,13 @@
+package com.stefvic.opentelemetry.showcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpentelemetryShowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpentelemetryShowcaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stefvic/opentelemetry/showcase/OpentelemetryShowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefvic.opentelemetry.showcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpentelemetryShowcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
